0d8054692778ff9e01d22c914de178b7b0092346
Mark Stacy <mbstacy@gmail.com>
2018-02-05 15:25:54 -0700

    updated queue with initial load solr funcion.

--- geoblacklightq/tasks/tasks.py
+++ geoblacklightq/tasks/tasks.py
@@ -3,10 +3,10 @@ from dockertask import docker_task
 from subprocess import call,STDOUT
 import requests
 
-#Default base directory 
+#Default base directory
 #basedir="/data/static/"
 
-
+solr_connection="http://geoblacklight_solr:8983/solr/"
 #Example task
 @task()
 def add(x, y):
@@ -16,3 +16,12 @@ def add(x, y):
     """
     result = x + y
     return result
+
+@task()
+def solr_index(items):
+    """
+    Provide a list of JSON items to be index within the
+    Geoportal Solr index.
+    args: items - List of json objects
+    """
+    solr = pysolr.Solr(solr_connection, timeout=10)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #import ez_setup
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
-setup(name='cybercomq',
+setup(name='geoblacklightq',
       version='0.0',
       packages= find_packages(),
 )
