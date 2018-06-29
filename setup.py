af77a3325b763ade3f428182f07639e21ca3b632
Mark Stacy <mbstacy@gmail.com>
2018-06-29 17:18:56 -0600

    updated version so python will update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='geoblacklightq',
-      version='0.0',
+      version='0.0.1',
       packages= find_packages(),
       install_requires=[
           'celery==3.1.22',
