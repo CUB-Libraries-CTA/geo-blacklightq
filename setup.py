e7cf5a6ead210731208d35dd36fb5b65a7cb1b34
Mark Stacy <mbstacy@gmail.com>
2019-02-27 14:22:35 -0700

    added mock for rasterio dependency

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #import ez_setup
-#ez_setup.use_setuptools()
+# ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='geoblacklightq',
       version='0.0.7.746',
-      packages= find_packages(),
-      package_data={'geoblacklightq':['tasks/templates/*.tmpl','geoblacklightq/tasks/templates/*.tmpl']},
+      packages=find_packages(),
+      package_data={'geoblacklightq': [
+          'tasks/templates/*.tmpl', 'geoblacklightq/tasks/templates/*.tmpl']},
       install_requires=[
           'celery==3.1.22',
           'pymongo==3.2.1',
@@ -13,10 +14,11 @@ setup(name='geoblacklightq',
           'jinja2',
           'gsconfig',
           'fiona',
+          'mock',
           'rasterio'
       ],
       include_package_data=True,
-)
+      )
 
 
-#'fiona==1.7.12',
+# 'fiona==1.7.12',
